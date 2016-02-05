da9da00a7a75175e0fd4f7cba8900a5a05dd5b73
Ben Bullock <benkasminbullock@gmail.com>
2016-02-05 12:59:05 +0900

    Initial commit of parse-diagnostics

---
+++ lib/Parse/Diagnostics.pm
@@ -0,0 +1,12 @@
+package Parse::Diagnostics;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+%EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+use warnings;
+use strict;
+use Carp;
+our $VERSION = '0.01';
+1;
---
+++ t/parse-diagnostics.t
@@ -0,0 +1,11 @@
+# This is a test for module Parse::Diagnostics.
+
+use warnings;
+use strict;
+use Test::More;
+use Parse::Diagnostics;
+
+done_testing ();
+# Local variables:
+# mode: perl
+# End:
