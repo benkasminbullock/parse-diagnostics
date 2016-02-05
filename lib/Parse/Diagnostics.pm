18dfe2fda530959c8ea1156d7f2e01a8346260f6
Ben Bullock <benkasminbullock@gmail.com>
2016-02-05 13:47:26 +0900

    Implement some basic functionality using regexes.

--- lib/Parse/Diagnostics.pm
+++ lib/Parse/Diagnostics.pm
@@ -1,12 +1,64 @@
 package Parse::Diagnostics;
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT_OK = qw//;
+@EXPORT_OK = qw/parse_diagnostics $diagnostics_re/;
 %EXPORT_TAGS = (
     all => \@EXPORT_OK,
 );
 use warnings;
 use strict;
 use Carp;
+use Path::Tiny;
 our $VERSION = '0.01';
+
+our $message_re = qr
+    /
+	(
+	    "([^"]|\\")+"
+	|
+	    '([^']|\\')+'
+	)
+	(?:,.*?)?
+    /x;
+
+our $diagnostics_re = qr
+    /
+	(
+	    croak
+	|
+	    carp
+	|
+	    die
+	|
+	    warn
+	|
+	    confess
+	|
+	    cluck
+	)
+	\s*
+	(
+	    \(
+	    $message_re
+	    \)
+	|
+	    $message_re
+	)
+    /x;
+
+sub parse_diagnostics
+{
+    my ($file, %options) = @_;
+    my $contents = path ($file)->slurp ();
+    my @diagnostics;
+    while ($contents =~ /$diagnostics_re/g) {
+	push @diagnostics, {
+	    type => $1,
+	    message => $2,
+	    line => 0,
+	};
+    }
+    return \@diagnostics;
+}
+
 1;
